79f3ed21c1626f279aab9ce275b2f173a928d06b
Anurag Bhagsain <abhagsain@gmail.com>
2019-01-09 15:41:51 +0530

    update .gitignore

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 
 mongoose.connect(
-  process.env.DATABASEURL,  
-  {
+  process.env.DATABASEURL, {
     useNewUrlParser: true
   }
 );
