317ed927719c3993b9a1ba417675d89b9a6c4b60
Anurag Bhagsain <abhagsain@gmail.com>
2019-01-11 00:41:19 +0530

    add user profile feature to every user

--- routers/users.js
+++ routers/users.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const User = require('../models/user');
+const Campgrounds = require('../models/campgrounds');
 
 const router = express.Router();
 router.get('/profile/:id', (req, res) => {
-    res.send('This is an users page');
+    // Find the User in the database and then show his profile and created campgrounds by him.
+    User.findById(req.params.id, (err, foundUser) => {
+        if (!err) {
+            // eslint-disable-next-line no-underscore-dangle
+            Campgrounds.find().where('createdBy.id').equals(foundUser._id)
+                .exec((_err, userCampgrounds) => {
+                    if (!_err) {
+                        console.log(userCampgrounds);
+                        res.render('./users/show', {
+                            userCampgrounds,
+                            foundUser
+                        });
+                    } else {
+                        console.log(`No campgrounds found by the user ${_err}`);
+                    }
+                });
+        } else {
+            res.render('notFound');
+        }
+    });
 });
 module.exports = router;
